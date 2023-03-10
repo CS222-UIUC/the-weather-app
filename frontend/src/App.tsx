a88a254223a5e721d24209a5680820fb79ae363e
Skywalker31415 <eli.gidron@gmail.com>
2023-03-10 17:40:18 -0600

    added basic boxes and layout from Figma

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,43 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                App Name Here
             </header>
+            <div className='Search-box'>
+                Search
+            </div>
+            <div className='Graphs-header'>
+                Header
+            </div>
+            <div className='Graph-box'>
+                Graphs Here
+            </div>
+            <div className='Main-weather-box'>
+                <div className='Main-weather-col'>
+                    <div className='Main-weather-row'>
+                        70°
+                    </div>
+                    <div className='Main-weather-row'>
+                        H:74°, L:65°
+                    </div>
+                </div>
+                <div className='Main-weather-col'>
+                    Info here
+                </div>
+            </div>
+            <div className='Scrollable-daily-forecast'>
+                Daily Forecast Here
+            </div>
+            <div className='Scrollable-weekly-forecast'>
+                Weekly Forecast Here
+            </div>
+            <div className='Bottom-box'>
+                Warnings and Advice Here
+            </div>
         </div>
     )
 }
